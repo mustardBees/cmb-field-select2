d3d4f0e103915b55cc986d352e4230c625d29bf1
Phil Wylie <philw@iweb.co.uk>
2014-02-09 16:33:23 +0000

    Add single value select box. Improved JS consistency. Blend in better with WordPress 3.8 design refresh. Support CMB 1.0.1. Basic documentation.

--- cmb-field-select2.php
+++ cmb-field-select2.php
@@ -1,58 +1,86 @@
 <?php
-	
-	/*
-		Plugin Name: CMB Field Type: Select2
-		Plugin URI: https://github.com/mustardBees/cmb-field-select2
-		Description: Select2 field type for Custom Metaboxes and Fields for WordPress
-		Version: 0.1
-		Author: Phil Wylie
-		Author URI: http://www.philwylie.co.uk/
-	*/
-	
-	
-	
-	/**
-	 * Render our interface
-	 */
-	function pw_select2_field( $field, $meta ) {
-		wp_enqueue_script( 'pw-select2-field-js', plugin_dir_url( __FILE__ ) . 'js/select2/select2.min.js', array( 'jquery-ui-sortable' ), '3.2' );
-		wp_enqueue_script( 'pw-select2-field-init', plugin_dir_url( __FILE__ ) . 'js/select2-init.js', array( 'pw-select2-field-js' ), '1.0' );
-		wp_enqueue_style( 'pw-select2-field-css', plugin_dir_url( __FILE__ ) . 'js/select2/select2.css' );
-		wp_enqueue_style( 'pw-select2-field-mods', plugin_dir_url( __FILE__ ) . 'css/select2.css' );
-		
-		$options = array();
-		
-		if ( isset( $field['options'] ) && ! empty( $field['options'] ) ) {
-			foreach ( $field['options'] as $option ) {
-				$options[] = array(
-					'id' => $option['value'],
-					'text' => $option['name']
-				);
-			}
+/*
+Plugin Name: CMB Field Type: Select2
+Plugin URI: https://github.com/mustardBees/cmb-field-select2
+Description: Select2 field type for Custom Metaboxes and Fields for WordPress
+Version: 0.2
+Author: Phil Wylie
+Author URI: http://www.philwylie.co.uk/
+License: GPLv2+
+*/
+
+// Useful global constants
+define( 'PW_SELECT2_URL', plugin_dir_url( __FILE__ ) );
+
+/**
+ * Enqueue scripts and styles, call requested select box field
+ */
+function pw_select2( $field, $meta ) {
+	wp_enqueue_script( 'pw-select2-field-js', PW_SELECT2_URL . 'js/select2/select2.min.js', array( 'jquery-ui-sortable' ), '3.4.5' );
+	wp_enqueue_script( 'pw-select2-field-init', PW_SELECT2_URL . 'js/select2-init.js', array( 'pw-select2-field-js' ), null );
+	wp_enqueue_style( 'pw-select2-field-css', PW_SELECT2_URL . 'js/select2/select2.css', array(), '3.4.5' );
+	wp_enqueue_style( 'pw-select2-field-mods', PW_SELECT2_URL . 'css/select2.css', array(), null );
+
+	call_user_func( $field['type'], $field, $meta );
+
+	echo ( isset( $field['desc'] ) && ! empty( $meta ) ? '<p class="cmb_metabox_description">' . $field['desc'] . '</p>' : '' );
+}
+add_filter( 'cmb_render_pw_select', 'pw_select2', 10, 2 );
+add_filter( 'cmb_render_pw_multiselect', 'pw_select2', 10, 2 );
+
+/**
+ * Render select box field
+ */
+function pw_select( $field, $meta ) {
+	echo '<select name="', $field['id'], '" id="', $field['id'], '" data-placeholder="' . $field['desc'] . '" class="select2">';
+	echo '<option></option>';
+	if ( isset( $field['options'] ) && ! empty( $field['options'] ) ) {
+		foreach ( $field['options'] as $option_key => $option ) {
+			$label = isset( $option['name'] ) ? $option['name'] : $option;
+			$value = isset( $option['value'] ) ? $option['value'] : $option_key;
+
+			echo '<option value="', $value, '" ', selected( $meta == $value ) ,'>', $label, '</option>';
 		}
-		
-		wp_localize_script( 'pw-select2-field-init', $field['id'] . '_data', $options );
-		
-		if ( ! empty( $meta ) ) {
-			$meta = implode( ',', $meta );
-		}
-		
-		echo '<input type="hidden" name="' . $field['id'] . '" id="' . $field['id'] . '" data-placeholder="' . $field['desc'] . '" class="select2" value="' . $meta . '" />';
 	}
-	add_filter( 'cmb_render_select2', 'pw_select2_field', 10, 2 );
-	
-	
-	/**
-	 * Split latitude/longitude values into two meta fields
-	 */
-	function pw_select2_field_validation( $new ) {
-		if ( empty( $new ) ) {
-			$new = '';
-		} else {
-			$new = explode( ',', $new );
+	echo '</select>';
+}
+
+/**
+ * Render multi-value select box field
+ */
+function pw_multiselect( $field, $meta ) {
+	$options = array();
+
+	if ( isset( $field['options'] ) && ! empty( $field['options'] ) ) {
+		foreach ( $field['options'] as $option_key => $option ) {
+			$label = isset( $option['name'] ) ? $option['name'] : $option;
+			$value = isset( $option['value'] ) ? $option['value'] : $option_key;
+
+			$options[] = array(
+				'id' => $value,
+				'text' => $label
+			);
 		}
-		
-		return $new;
 	}
-	add_filter( 'cmb_validate_select2', 'pw_select2_field_validation', 10, 3 );
-	
+
+	wp_localize_script( 'pw-select2-field-init', $field['id'] . '_data', $options );
+
+	if ( ! empty( $meta ) ) {
+		$meta = implode( ',', $meta );
+	}
+
+	echo '<input type="hidden" name="' . $field['id'] . '" id="' . $field['id'] . '" data-placeholder="' . $field['desc'] . '" class="select2" value="' . $meta . '" />';
+}
+
+/**
+ * Handle saving of single and multi-value select fields
+ */
+function pw_select2_sanitise( $meta_value, $field ) {
+	if ( empty( $meta_value ) ) {
+		$meta_value = '';
+	} elseif ( 'pw_multiselect' == $field['type'] ) {
+		$meta_value = explode( ',', $meta_value );
+	}
+
+	return $meta_value;
+}
