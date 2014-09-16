cc3cb87414923e656b4238123c6f6b34f2f6e3d0
pasqualevitiello <pasqualevitiello@gmail.com>
2014-09-16 17:19:24 +0200

    Fix selectors to make it work with latest CMB2

--- js/select2-init.js
+++ js/select2-init.js
@@ -1,12 +1,12 @@
 (function( $ ) {
 
-	$( '.cmb-type-pw_select .select2' ).each(function() {
+	$( '.cmb-type-pw-select .select2' ).each(function() {
 		$( this ).select2({
 			allowClear: true
 		});
 	});
 
-	$( '.cmb-type-pw_multiselect .select2' ).each(function() {
+	$( '.cmb-type-pw-multiselect .select2' ).each(function() {
 		var instance = $( this ),
 			instance_data = window[ instance.attr( 'id' ) + '_data' ];
 
